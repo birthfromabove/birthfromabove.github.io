01d492b180fd806bc6340dad103bc75017f13a24
birthfromabove <132463796+birthfromabove@users.noreply.github.com>
2025-02-25 17:41:45 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,18 +1,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-// Получаем все элементы div с классом 'random-div'
-const divs = document.querySelectorAll('.random-div');
-// Проверяем, есть ли на странице какие-либо div-ы
+const divs = document.querySelectorAll('.poetry-quote');
 if (divs.length > 0) {
-// Выбираем случайный индекс
 const randomIndex = Math.floor(Math.random() * divs.length);
-// Получаем случайный div
 const randomDiv = divs[randomIndex];
-// Отображаем случайный div
 randomDiv.classList.remove('hidden');
 }
 });
 document.addEventListener('DOMContentLoaded', (event) => {
-const divs = document.querySelectorAll('.random-div2');
+const divs = document.querySelectorAll('.biblical-quote');
 if (divs.length > 0) {
 const randomIndex = Math.floor(Math.random() * divs.length);
 const randomDiv = divs[randomIndex];
